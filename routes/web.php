0eb0d137d84a4620cc97fd9136ce5da72b4a485b
Bangik <aldi.novaldi.frmnsyah@gmail.com>
2022-03-22 16:40:13 +0700

    Assignment 7

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('/about', [HomeController::class, 'about'])->name('home.about');
+Route::get('/contact', [HomeController::class, 'contact'])->name('home.contact');
+Route::get('/product', [HomeController::class, 'product'])->name('home.product');
+Route::get('/category', [HomeController::class, 'category'])->name('home.category');
 
 Route::resource('user', UserController::class);
 
